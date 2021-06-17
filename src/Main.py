b85741f1891b12043a6f83a58df9297c79e65c45
Felipe Oliveira <flpdias14@gmail.com>
2021-06-17 15:37:30 -0300

    chore: extract values from a .txt file and save to .csv

--- src/Main.py
+++ src/Main.py
@@ -3,50 +3,89 @@
 import pandas as pd
 
 
-filename = '/home/felipe/Área de trabalho/resultados docker final/com networkmanager/felipe/stap-networkmanager.txt'
+filename = 'fragmentacao.txt'
+
+file = open(filename, "r") # read the file
+
+day = 0 # day controller
+
+previous_day = '' # controls when the day change
+
+# list to add the processes and ocurrences
+days = []
+process_names = []
+process_ids = []
+process_fathers = []
+process_father_ids = []
+uids = []
+ocurrences = []
+
+for register in file: # for each register (line) in file
+
+    if "Mar" in register: # checking if the register has the month tag
+        if previous_day != register.split(' ')[2]: # verify if the day changed
+            day += 1
+            previous_day = register.split(' ')[2]
+
+    if "#" in register: # verify if the register has the stap tag
+        days.append(day)
+
+        aux = register.split('/')[0].split('(')
+
+        process_names.append(aux[0])
+
+        if len(aux) == 2:
+            process_ids.append(aux[1].replace(')', ''))
+        else:
+            process_ids.append('')
+
+        aux = register.split('/')[1].replace("Pai: ", "").split('(')
+        
+        process_fathers.append(aux[0])
+
+        if len(aux) == 2:
+            process_father_ids.append(aux[1].replace(')', ''))
+        else:
+            process_father_ids.append('')
+
+        uids.append(register.split('/')[2].replace("UID: ", "").split('#')[0])
+
+        if len(register.split('/')[2].replace("UID: ", "").split('#')) == 2:
+            ocurrences.append(register.split('/')[2].replace("UID: ", "").split('#')[1].replace(",\n", ""))
+        else:
+             ocurrences.append(0)
+print(" days: {}, process: {}, processid: {}, father: {}, fatherid: {}, uid: {}, ocurrences: {} ".format(len(days), len(process_names), len(process_ids), len(process_fathers), len(process_father_ids), len(uids), len(ocurrences)))
+
+# dictionary of lists  
+dict = {'day': days, 'process': process_names, 'process_id': process_ids, 'father': process_fathers, 'father_id': process_father_ids, 'uid': uids, 'ocurrences': ocurrences}
+
+
 
 # cria um dataframe com os dados
-df = pd.read_csv(filename, sep="\n", header=None)
+df = pd.DataFrame(dict)
 
-# adiciona o nome da coluna
-df.columns = ['process']
+df.to_csv("pre_processed_"+filename.replace('txt', 'csv'))
 
-df_filtered =  df[df.process.str.contains('#')]
-
-
-registers = df_filtered['process'].str.replace('[\[\]]', '').str.split(',');
-
-separated_process = []
-for register in registers:
-    register.remove(register[0])
-    for r in register:
-        separated_process.append(r)
-        
-separated_process = pd.Series(separated_process).str.split('#')
-
-df['process'] = separated_process.str.get(0)
-df['ocurences'] = separated_process.str.get(1).astype(float)
-signatures= df['process'].unique()
-
-# print df[df['process'].str.contains('NetworkManager')]['ocurences'].head().sum(axis=1)
-
-groupby_process =  df.groupby('process', as_index=False)['ocurences'].agg(['count', 'min', 'mean', 'max', 'sum'])
-groupby_process = groupby_process.sort_values('sum', ascending=False)
-# groupby_process =  df.groupby('process')
-print groupby_process
-# net =  groupby_process.get_group('NetworkManager(402)/Pai: systemd(1)/UID: 0')
-# net.ocurences.astype(int)
-# print net.dtypes()
-list_process = []
-list_ocurences = []
-# for process, process_df in groupby_process:
-#     list_process.append(process)
-#     list_ocurences.append(process_df[process_df['process']==process]['ocurences'].sum())
-# f = groupby_process.get_group('NetworkManager(402)/Pai: systemd(1)/UID: 0')
-# print f.ocurences.sum()
-
-# print list_process[0:5]
-# print list_ocurences[0:5]
+df['ocurrences'] = pd.to_numeric(df['ocurrences'])
 
 
-# print df.head()
+groupby_process =  df.groupby(['day','process'], as_index=False)['ocurrences'].agg(['count', 'min', 'mean', 'max', 'sum']).sort_values(['day', 'sum'], ascending=[True, False])
+# groupby_process = groupby_process.sort_values('sum', ascending=False)
+# # groupby_process =  df.groupby('process')
+groupby_process.to_csv("processed_groupby_day_and_process_"+filename.replace(".txt", ".csv"))
+# # net =  groupby_process.get_group('NetworkManager(402)/Pai: systemd(1)/UID: 0')
+# # net.ocurences.astype(int)
+# # print net.dtypes()
+# list_process = []
+# list_ocurences = []
+# # for process, process_df in groupby_process:
+# #     list_process.append(process)
+# #     list_ocurences.append(process_df[process_df['process']==process]['ocurences'].sum())
+# # f = groupby_process.get_group('NetworkManager(402)/Pai: systemd(1)/UID: 0')
+# # print f.ocurences.sum()
+
+# # print list_process[0:5]
+# # print list_ocurences[0:5]
+
+
+# # print df.head()
